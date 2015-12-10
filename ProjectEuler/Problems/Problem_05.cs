65f18fa3490401c6e8237e76f9359401318a94ca
GB <banicageorge@outlook.com>
2015-12-10 22:07:38 +0200

    Problem 5: Smallest multiple

---
+++ ProjectEuler/Problems/Problem_05.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    class Problem_05 : IProblems
+    {
+        public string Output { get; set; }
+
+        public string Input { get; set; }
+
+        public void run()
+        {
+            System.Diagnostics.Debug.WriteLine("running problem 05");
+            run_solution01();
+            System.Diagnostics.Debug.WriteLine("ended problem 05");
+        }
+
+        private void run_solution01()
+        {
+            int n = Int32.Parse(Input);
+            bool found = false;
+            long i = 2;
+
+            while (found == false)
+            {
+                bool all = true;
+                for (int k = 2; k < 21; k++)
+                    if (i % k != 0)
+                    {
+                        all = false;
+                        break;
+                    }
+                i += 2; 
+                found = all;
+            }
+
+            Output = (i-2).ToString();
+        }
+
+    }
+}
